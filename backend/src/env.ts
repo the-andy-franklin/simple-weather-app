d463159650b46ca24ff31f8bcfb5626b5de99839
Andy <the.andy.franklin@gmail.com>
2024-07-13 14:38:07 -0700

    swapped out redis for kv-store

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 import { load } from "dotenv";
 
-const env_vars = await load();
+await load({ export: true });
 
 export const env = z.object({
 	OPEN_WEATHER_MAP_API_KEY: z.string({ message: "OPEN_WEATHER_MAP_API_KEY is required" }),
-}).parse(env_vars);
+}).parse(Deno.env.toObject());
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,8 @@ import { z } from "zod";
 import { Try } from "fp-try";
 import { env } from "../src/env.ts";
 import { weather_schema } from "./validators/weather-schema.ts";
-import { redis } from "../src/redis.ts";
+
+const kv = await Deno.openKv();
 
 export const app = new Hono();
 app.use(cors());
@@ -22,13 +23,9 @@ app.post("/weather", async (c) => {
 	if (parsed.failure) return c.json({ error: parsed.error.message }, 400);
 	const { lat, lng } = parsed.data;
 
-	// Redis is only being used during development so I don't blow through my API limit
-	// It probably won't be needed/included in the final solution
-	const cache_key = `weather-${lat}-${lng}`;
-	if (redis) {
-		const cached_weather = await redis.get(cache_key);
-		if (cached_weather) return c.text(cached_weather);
-	}
+	// KV-store is only being used during development so I don't blow through my API limit
+	const cached_weather = await kv.get<string>([lat, lng]);
+	if (cached_weather.value) return c.text(cached_weather.value);
 
 	const weather_response = await Try(() =>
 		axios.get<unknown>(
@@ -37,7 +34,7 @@ app.post("/weather", async (c) => {
 	);
 	if (weather_response.failure) return c.json({ error: weather_response.error.message }, 500);
 
-	if (redis) redis.set(cache_key, JSON.stringify(weather_response.data), { ex: 60 * 60 * 4 });
+	await kv.set([lat, lng], JSON.stringify(weather_response.data), { expireIn: 1000 * 60 * 60 * 4 });
 	return c.json(weather_response.data);
 });
 
